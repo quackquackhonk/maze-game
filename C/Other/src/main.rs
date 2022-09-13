995d89d0028f337f08c11b10646ccc417d99a34b
zoomiti <sotoluis90@gmail.com>
2022-09-13 11:03:10 -0400

    Added homework C

---
+++ C/Other/src/main.rs
@@ -0,0 +1,52 @@
+#![allow(non_snake_case)]
+
+use std::io;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+enum Vertical {
+    UP,
+    DOWN,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+enum Horizontal {
+    LEFT,
+    RIGHT,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Corner {
+    vertical: Vertical,
+    horizontal: Horizontal,
+}
+
+impl ToString for Corner {
+    fn to_string(&self) -> String {
+        String::from(match (self.vertical, self.horizontal) {
+            (Vertical::UP, Horizontal::LEFT) => "\"┘\"",
+            (Vertical::UP, Horizontal::RIGHT) => "\"└\"",
+            (Vertical::DOWN, Horizontal::LEFT) => "\"┐\"",
+            (Vertical::DOWN, Horizontal::RIGHT) => "\"┌\"",
+        })
+    }
+}
+
+fn main() {
+    //let mut buf = String::new();
+    //let file_size = io::stdin().read_to_string(&mut buf).unwrap();
+
+    //println!("{buf}")
+    //let test = r#"{"vertical" : "UP", "horizontal" : "LEFT" }{"vertical" : "DOWN", "horizontal" : "RIGHT" }"#;
+
+    //test.split_inclusive("}");
+    let deserializer = serde_json::Deserializer::from_reader(io::stdin().lock());
+    let iterator = deserializer.into_iter::<Corner>();
+
+    println!("[");
+    for value in iterator {
+        println!("{},", value.unwrap().to_string());
+    }
+    println!("]")
+}
