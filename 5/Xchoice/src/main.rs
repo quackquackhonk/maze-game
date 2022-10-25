502e289b7b973904e02072931bd9c37dec7c0651
Sahana Tankala <sahana@gmail.com>
2022-10-25 15:43:21 -0400

    Fixes parsing errors in `xchoice`

--- 5/Xchoice/src/main.rs
+++ 5/Xchoice/src/main.rs
@@ -1,9 +1,10 @@
 use std::io::{Read, Write};
 
 use common::grid::Position;
-use common::json::{Coordinate, JsonState, JsonStrategyDesignation};
-use common::BOARD_SIZE;
-use player::strategy::{NaiveStrategy, PlayerBoardState};
+use common::json::{Coordinate, JsonState};
+use common::{State, BOARD_SIZE};
+use player::json::JsonStrategyDesignation;
+use player::strategy::NaiveStrategy;
 use serde::{Deserialize, Serialize};
 
 /// Enumerated Valid JSON input for `xchoice`
@@ -37,16 +38,8 @@ fn write_json_out_to_writer(output: impl Serialize, writer: &mut impl Write) -> 
     Ok(())
 }
 
-// fn execute_test<const COLS: usize, const ROWS: usize>(
-//     strat: NaiveStrategy,
-//     state: PlayerBoardState<COLS, ROWS>,
-//     goal: Position,
-// ) -> PlayerAction<COLS, ROWS> {
-//     strat.get_move(board_state, start, goal_tile)
-// }
-
 fn read_and_write_json(reader: impl Read, writer: &mut impl Write) -> Result<(), String> {
-    let input = get_json_iter_from_reader(reader)?;
+    let mut input = get_json_iter_from_reader(reader)?;
 
     let strat: NaiveStrategy<BOARD_SIZE, BOARD_SIZE> =
         match input.next().ok_or("No valid JSON Strategy found")? {
@@ -54,7 +47,7 @@ fn read_and_write_json(reader: impl Read, writer: &mut impl Write) -> Result<(),
             _ => Err("StrategyDesignation was not the first json input found")?,
         };
 
-    let state: PlayerBoardState<BOARD_SIZE, BOARD_SIZE> =
+    let state: State<BOARD_SIZE, BOARD_SIZE> =
         match input.next().ok_or("No valid State JSON found")? {
             ValidJson::State(state) => state.into(),
             _ => Err("State was not the second json input found")?,
