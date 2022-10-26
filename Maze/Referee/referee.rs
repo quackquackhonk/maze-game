78a325759bbf66cc8c3e2b715c4d273df22ba2e8
Sahana Tankala <sahana@gmail.com>
2022-10-26 17:09:29 -0400

    Adds `Referee` skeleton

--- Maze/Referee/referee.rs
+++ Maze/Referee/referee.rs
@@ -1,3 +1,21 @@
+use players::player::Player;
+
+/// The Result of calling `Referee::run_game(...)`.
+/// - The `winners` field contains all the winning players.
+/// - The `losers` field contains all the players who misbehaved during the game.
+struct GameResult<P: Player> {
+    winners: Vec<P>,
+    losers: Vec<P>,
+}
+
+struct Referee {}
+
+impl Referee {
+    /// Runs the game given the age-sorted `Vec<impl Players>`, `players`.
+    pub fn run_game<P: Player>(&mut self, players: Vec<P>) -> GameResult<P> {
+        todo!();
+    }
+}
 
 fn main() {
     println!("Hello World!");
