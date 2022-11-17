b2dddaaf6458d82be4cd685d174075e2784e45e6
zoomiti <sotoluis90@gmail.com>
2022-11-17 15:51:26 -0500

    Add tests for setup in PlayerProxy

--- Maze/Remote/player.rs
+++ Maze/Remote/player.rs
@@ -116,3 +116,109 @@ impl<In: Read + Send, Out: Write + Send> PlayerApi for PlayerProxy<In, Out> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::ops::Deref;
+
+    use common::ColorName;
+    use serde_json::json;
+
+    use super::*;
+
+    impl<In: Read + Send, Out: Write + Send> PlayerProxy<In, Out> {
+        fn get_output(&self) -> impl Deref<Target = Out> + '_ {
+            self.out.borrow()
+        }
+    }
+
+    #[test]
+    fn test_new() {
+        let player = PlayerProxy::new(Name::from_static("john"), "".as_bytes(), Vec::new());
+
+        assert_eq!(player.name().unwrap(), Name::from_static("john"));
+    }
+
+    #[test]
+    fn test_setup() {
+        let mut player = PlayerProxy::new(Name::from_static("joe"), "\"void\"".as_bytes(), vec![]);
+
+        player.setup(None, (1, 2)).expect("Should not error");
+        assert_eq!(
+            serde_json::to_string(&JsonFunctionCall::setup(None, (1, 2)))
+                .unwrap()
+                .as_bytes(),
+            &*player.get_output()
+        );
+        assert_eq!(
+            serde_json::to_value(&JsonFunctionCall::setup(None, (1, 2))).unwrap(),
+            json!(["setup", [false, { "row#":2, "column#": 1 }]])
+        );
+
+        let mut player = PlayerProxy::new(Name::from_static("joe"), "\"void\"".as_bytes(), vec![]);
+        let state = State {
+            player_info: vec![PubPlayerInfo {
+                current: (0, 0),
+                home: (1, 1),
+                color: ColorName::Red.into(),
+            }]
+            .into(),
+            ..Default::default()
+        };
+        player
+            .setup(Some(state.clone()), (3, 4))
+            .expect("Setting up should not error");
+
+        assert_eq!(
+            serde_json::to_string(&JsonFunctionCall::setup(Some(state.clone()), (3, 4)))
+                .unwrap()
+                .as_bytes(),
+            &*player.get_output()
+        );
+        assert_eq!(
+            serde_json::to_value(&JsonFunctionCall::setup(Some(state), (3, 4))).unwrap(),
+            json!(["setup", [
+                {
+                    "board": {
+                        "connectors": [
+                            [ "─", "│", "└", "┌", "┐", "┘", "┴" ],
+                            [ "├", "┬", "┤", "┼", "─", "│", "└" ],
+                            [ "┌", "┐", "┘", "┴", "├", "┬", "┤" ],
+                            [ "┼", "─", "│", "└", "┌", "┐", "┘" ],
+                            [ "┴", "├", "┬", "┤", "┼", "─", "│" ],
+                            [ "└", "┌", "┐", "┘", "┴", "├", "┬" ],
+                            [ "┤", "┼", "─", "│", "└", "┌", "┐" ] 
+                        ],
+                        "treasures": [
+                            [ [ "alexandrite-pear-shape", "alexandrite" ], [ "alexandrite", "amethyst" ], [ "almandine-garnet", "ammolite" ], [ "amethyst", "aplite" ], [ "ametrine", "aquamarine" ], [ "ammolite", "aventurine" ], [ "apatite", "beryl" ] ],
+                            [ [ "aplite", "black-onyx" ], [ "apricot-square-radiant", "blue-ceylon-sapphire" ], [ "aquamarine", "blue-pear-shape" ], [ "australian-marquise", "bulls-eye" ], [ "aventurine", "chrome-diopside" ], [ "azurite", "chrysolite" ], [ "beryl", "citrine" ] ],
+                            [ [ "black-obsidian", "color-change-oval" ], [ "black-onyx", "diamond" ], [ "black-spinel-cushion", "emerald" ], [ "blue-ceylon-sapphire", "garnet" ], [ "blue-cushion", "goldstone" ], [ "blue-pear-shape", "gray-agate" ], [ "blue-spinel-heart", "green-beryl-antique" ] ],
+                            [ [ "bulls-eye", "green-princess-cut" ], [ "carnelian", "hackmanite" ], [ "chrome-diopside", "hematite" ], [ "chrysoberyl-cushion", "jasper" ], [ "chrysolite", "kunzite-oval" ], [ "citrine-checkerboard", "labradorite" ], [ "citrine", "lemon-quartz-briolette" ] ],
+                            [ [ "clinohumite", "mexican-opal" ], [ "color-change-oval", "morganite-oval" ], [ "cordierite", "orange-radiant" ], [ "diamond", "padparadscha-sapphire" ], [ "dumortierite", "pink-emerald-cut" ], [ "emerald", "pink-round" ], [ "fancy-spinel-marquise", "prasiolite" ] ],
+                            [ [ "garnet", "purple-cabochon" ], [ "golden-diamond-cut", "purple-spinel-trillion" ], [ "goldstone", "raw-beryl" ], [ "grandidierite", "red-diamond" ], [ "gray-agate", "rhodonite" ], [ "green-aventurine", "rose-quartz" ], [ "green-beryl-antique", "ruby" ] ],
+                            [ [ "green-beryl", "spinel" ], [ "green-princess-cut", "stilbite" ], [ "grossular-garnet", "super-seven" ], [ "hackmanite", "tigers-eye" ], [ "heliotrope", "tourmaline" ], [ "hematite", "white-square" ], [ "iolite-emerald-cut", "yellow-beryl-oval" ] ]
+                        ]
+                    },
+                    "spare": {
+                        "tilekey": "┼",
+                        "1-image": "yellow-heart",
+                        "2-image": "yellow-jasper"
+                    },
+                    "plmt": [
+                        {
+                            "current": {"row#": 0, "column#": 0},
+                            "home": {"row#": 1, "column#": 1},
+                            "color": "red"
+                        }
+                    ],
+                    "last": null
+                },
+                { "row#":4, "column#": 3 }]])
+        );
+
+        // test no response
+        let mut player = PlayerProxy::new(Name::from_static("joe"), "".as_bytes(), vec![]);
+        assert!(player.setup(None, (0, 0)).is_err());
+    }
+}
