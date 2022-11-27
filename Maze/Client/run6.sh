1e02524a6fe2599775d37ffb5bc4bc6addbd4043
zoomiti <sotoluis90@gmail.com>
2022-11-27 17:13:40 -0500

    Creates Milestone 9 delivery directory

--- Maze/Client/run6.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-./client 10000 -n ava -s Riemann &
-./client 10000 -n bob -s Euclid &
-./client 10000 -n charlie -s Euclid &
-./client 10000 -n dan -s Riemann &
-./client 10000 -n ellen -s Riemann &
-./client 10000 -n fredrick -s Euclid
