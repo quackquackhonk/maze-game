d793bbdf33034f7cce62b1d762cad8453089e5b9
Sahana Tankala <sahana@gmail.com>
2022-11-30 23:33:40 -0500

    Makes `referee::Config` derive `Default`

--- Maze/Referee/config.rs
+++ Maze/Referee/config.rs
@@ -1,14 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Config {
-    /// If None use single goal model.
-    /// If Some() use multiple goals
     pub multiple_goals: bool,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            multiple_goals: false,
-        }
-    }
 }
