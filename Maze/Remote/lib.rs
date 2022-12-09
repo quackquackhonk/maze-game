42daa56995b170f51193e8bd7b65f8e03af4464e
Sahana Tankala <sahanatankala@gmail.com>
2022-12-08 23:43:20 -0500

    Adds library layout to `remote` README

--- Maze/Remote/lib.rs
+++ Maze/Remote/lib.rs
@@ -2,6 +2,20 @@
 //!
 //! # Library Layout
 //!
+//! ## Player
+//! Contains the definition for a [`player::PlayerProxy`], which enables the
+//! [`::referee::referee::Referee`] to communicate with `Client`s over a TCP connection. Also
+//! contains a [`players::player::PlayerApi`] implementation for [`player::PlayerProxy`] so it can
+//! be used in place of [`players::player::LocalPlayer`] in a [`::referee::player::Player`].
+//!
+//! ## Referee
+//! Contains the definition for a [`refreee::RefereeProxy`], which enables a `Client` to
+//! communicate with a remote `Server`.
+//!
+//! ## Json
+//! Contains the data definition for the JSON that is sent between the [`player::PlayerProxy`] and
+//! [`refreee::RefereeProxy`]. This module also has methods for conveniently constructing and
+//! accessing data inside [`json::JsonMethodCalls`].
 //!
 
 /// contains data defintions for remote messages
